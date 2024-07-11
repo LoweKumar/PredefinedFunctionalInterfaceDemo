8bc44cbe1fde0f0191713116761d4d20cf76fd77
Lowe Kumar <lowekr00@gmail.com>
2024-07-11 14:30:23 +0530

    First Commit for fucntional interface demo in git

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+import java.util.function.*;
+
+public class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks){
+        this.name= name;
+        this.marks=marks;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,35 @@
+import class Student;
+public class Test {
+    public static void main(String[] args){
+        Function<Student, String> f = s-> {
+            int marks = s.marks;
+            String grade = " ";
+            if (marks>=80) grade = "A grade";
+            else if (marks>=60) grade = "B grade";
+            else grade = "Fail";
+            return grade;
+        };
+        Predicate<Student> p = s-> s.marks >=60;
+
+        Consumer<Student> c = s1-> {
+            System.out.println("StudentName = "+ s1.name);
+            System.out.println("StudentMarks = "+ s1.marks);
+            System.out.println("StudentGrade = "+ f.apply(s1));
+            System.out.println();
+        };
+
+
+        Student[] s = {new Student("Durga", 95),
+                        new Student("Ravi", 65),
+                        new Student("Sam", 55)
+                        };
+
+        for(Student s1 : s){
+            if(p.test(s1)){
+                c.accept(s1);
+            }
+        }
+
+
+    }
+}
